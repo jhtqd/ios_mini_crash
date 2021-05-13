fe25754ee9db840b59eeb4da911b4ff6ae6247e0
jhtqd <jht_qd@163.com>
2021-05-13 19:17:32 +0800

    清理一下

--- Tests/ios_mini_crashTests/ios_mini_crashTests.swift
+++
@@ -1,9 +0,0 @@
-    import XCTest
-    @testable import ios_mini_crash
-
-    final class ios_mini_crashTests: XCTestCase {
-        func testExample() {
-//            XCTAssertEqual(ios_mini_crash().text, "Hello, World!")
-            CrashManager.instance.installUncaughtExceptionHandler()
-        }
-    }
